887795220bf748b351f203cbd133524bba5c1db2
Murad Emirgamzaev <murad2013408@gmail.com>
2026-06-29 14:13:15 +0500

    Scaffold project architecture

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    additionalData: `@use "@/styles/helpers" as *;`,
+    silenceDeprecations: ["legacy-js-api"],
+  },
 };
 
 export default nextConfig;
