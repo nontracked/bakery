8bcfed2f9b5417b8dc0bd1e56dc42478a7d30c98
Murad Emirgamzaev <murad2013408@gmail.com>
2026-08-08 19:49:46 +0500

    add:checkout page done

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -3,11 +3,51 @@ import './Checkout.scss'
 import {CheckoutForm} from "@/components/CheckoutForm";
 import {useRouter} from "next/navigation";
 import {CheckoutCart} from "@/components/CheckoutCart";
+import {useCartStore} from "@/store/useCartStore";
+import {SubmitHandler} from "react-hook-form";
+import {checkoutSchema} from "@/schemas/checkoutSchema";
+import {z} from "zod";
+import {createOrder} from "@/actions/checkout";
 
+type CheckoutFormValues = z.infer<typeof checkoutSchema>
+export type Payload = {
+  customerName: string;
+  phone: string;
+  address: string;
+  email: string;
+  promocode: string | null;
+  comment: string | null;
+  items: {
+    productId: string;
+    quantity: number
+  }[];
+}
 export const Checkout = () => {
   const router = useRouter()
-  const handleSubmit = () => {
-    console.log('данные собраны')
+  const cart = useCartStore(state => state.cart)
+  const appliedPromocode = useCartStore(state => state.appliedPromocode)
+  const handleSubmit: SubmitHandler<CheckoutFormValues> = async (formData) => {
+    if (!cart || cart.length === 0) return
+
+    // Цены мы специально вырезаем, чтобы сервер сам их посчитал
+    const itemsForBackend = cart.map(({id, quantity}) => ({
+      productId: id,
+      quantity
+    }))
+    const payload: Payload = {
+      customerName: `${formData.firstName} ${formData.lastName || ''}`.trim(),
+      phone: formData.phone,
+      address: formData.address,
+      email: formData.email,
+      comment: formData.comment || null,
+      promocode: appliedPromocode || null,
+      items: itemsForBackend,
+    }
+    try {
+      await createOrder(payload)
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   return (
