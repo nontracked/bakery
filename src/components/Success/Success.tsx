786d65b7a8c0efbfb138fe1a1a46cc06df12280f
Murad Emirgamzaev <murad2013408@gmail.com>
2026-08-11 18:13:20 +0500

    add:clear cart & form after payout

--- src/components/Success/Success.tsx
+++ src/components/Success/Success.tsx
@@ -1,10 +1,11 @@
 'use client'
 import './Success.scss'
 import Image from "next/image";
-import React from "react";
+import React, {useEffect} from "react";
 import {useRouter} from "next/navigation";
 import {TAXES_VALUE} from "@/lib/constants";
 import {formatPrice} from "@/utils/formatPrice";
+import {useCartStore} from "@/store/useCartStore";
 
 interface Props {
   orderId: string,
@@ -14,6 +15,7 @@ interface Props {
 }
 
 export const Success = ({orderId, paymentTime, clientName, totalPrice}: Props) => {
+  const clearCart = useCartStore((state) => state.clearCart)
   const totalFormatted = formatPrice(totalPrice)
   const serviceFeeFormatted = formatPrice(totalPrice * TAXES_VALUE)
   const subTotalPrice = formatPrice(totalPrice - (totalPrice * TAXES_VALUE))
@@ -28,6 +30,10 @@ export const Success = ({orderId, paymentTime, clientName, totalPrice}: Props) =
     {label: 'Amount', value: `$ ${subTotalPrice}`},
     {label: 'Service Fee', value: `$ ${serviceFeeFormatted}`},
   ]
+  useEffect(() => {
+    clearCart()
+    sessionStorage.removeItem('checkout-draft')
+  }, []);
   return (
     <div className="success">
       <div className="success__wrap">
