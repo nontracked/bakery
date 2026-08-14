5e022ef8ff1c53fe4051096e3d965fa5a9c9a2c7
Murad Emirgamzaev <murad2013408@gmail.com>
2026-08-14 14:49:43 +0500

    add:script to package.json for update version

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,10 +1,11 @@
+import packageInfo from '../../../package.json';
 import './Footer.scss'
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
     <footer className="footer">
-      <p>{currentYear} / dev M. E.</p>
+      <p>{currentYear} / dev M. E. / v {packageInfo.version}</p>
     </footer>
   )
 }
